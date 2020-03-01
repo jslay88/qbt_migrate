966269c0e906df97151000769afb6b6ffbf3d65d
Justin Slay <justin.slay@gmail.com>
2020-03-01 16:35:52 -0700

    Refactor - Removed trailing slashes issue

--- qbt_migrate/classes.py
+++ qbt_migrate/classes.py
@@ -1,46 +1,23 @@
 import os
 import re
-import sys
 import logging
 import zipfile
 
 from datetime import datetime
 
+from .methods import discover_bt_backup_path, convert_slashes
+
 
 logger = logging.getLogger(__name__)
 
 
-def valid_path(path: str):
-    if sys.platform.startswith('win32') and not path.endswith('\\'):
-        return path + '\\'
-    if sys.platform.startswith('linux') and not path.endswith('/'):
-        return path + '/'
-    return path
-
-
-def convert_slashes(path: str, target_os: str):
-    if target_os.lower() not in ('windows', 'linux', 'mac'):
-        raise ValueError('Target OS is not valid. Must be Windows, Linux, or Mac. Received: %s' % target_os)
-    if target_os.lower() == 'windows':
-        logger.debug('Convert to Windows Slashes')
-        return path.replace('/', '\\')
-    logger.debug('Convert to Unix Slashes')
-    return path.replace('\\', '/')
-
-
 class QBTBatchMove(object):
+    logger = logging.getLogger(__name__ + '.QBTBatchMove')
+
     def __init__(self, bt_backup_path: str = None):
         if bt_backup_path is None:
-            logger.debug('Discovering BT_backup path...')
-            if sys.platform.startswith('win32'):
-                logger.debug('Windows System')
-                bt_backup_path = os.path.join(os.getenv('localappdata'), 'qBittorrent\\BT_backup')
-            elif sys.platform.startswith('linux'):
-                logger.debug('Linux System')
-                bt_backup_path = os.path.join(os.getenv('HOME'), '.local/share/data/qBittorrent/BT_backup')
-        if not os.path.exists(bt_backup_path) or not os.path.isdir(bt_backup_path):
-            raise NotADirectoryError(bt_backup_path)
-        logger.debug('BT_backup Path: %s' % bt_backup_path)
+            bt_backup_path = discover_bt_backup_path()
+        self.logger.debug('BT_backup Path: %s' % bt_backup_path)
         self.bt_backup_path = bt_backup_path
         self.discovered_files = None
 
@@ -56,21 +33,21 @@ class QBTBatchMove(object):
         :param create_backup: Create a backup of the file before modifying?
         :type create_backup: bool
         """
+        if not os.path.exists(self.bt_backup_path) or not os.path.isdir(self.bt_backup_path):
+            raise NotADirectoryError(self.bt_backup_path)
         if create_backup:
             backup_filename = 'fastresume_backup' + datetime.now().strftime('%Y%m%d%H%M%S') + '.zip'
             self.backup_folder(self.bt_backup_path,
                                os.path.join(os.path.dirname(self.bt_backup_path), backup_filename))
-        existing_path = valid_path(existing_path)
-        new_path = valid_path(new_path)
-        logger.info('Searching for .fastresume files with path %s ...' % existing_path)
+        self.logger.info('Searching for .fastresume files with path %s ...' % existing_path)
         self.discovered_files = self.discover_relevant_fast_resume(self.bt_backup_path,
                                                                    existing_path)
         if not self.discovered_files:
             raise ValueError('Found no .fastresume files with existing path %s' % existing_path)
-        logger.info('Found %s Files.' % len(self.discovered_files))
-        logger.debug('Discovered FastResumes: %s' % self.discovered_files)
+        self.logger.info('Found %s Files.' % len(self.discovered_files))
+        self.logger.debug('Discovered FastResumes: %s' % self.discovered_files)
         for file in self.discovered_files:
-            logger.info('Updating File %s...' % file.file_path)
+            self.logger.info('Updating File %s...' % file.file_path)
             new_save_path = file.save_path.replace(bytes(existing_path, 'utf-8'),
                                                    bytes(new_path, 'utf-8'))
             file.set_save_path(new_save_path.decode('utf-8'),
@@ -84,7 +61,7 @@ class QBTBatchMove(object):
                                    save_file=False,
                                    create_backup=False)
             file.save()
-            logger.info('File (%s) Updated!' % file.file_path)
+            self.logger.info('File (%s) Updated!' % file.file_path)
 
     @staticmethod
     def discover_relevant_fast_resume(bt_backup_path: str, existing_path: str):
@@ -105,16 +82,17 @@ class QBTBatchMove(object):
                              or existing_path in file.qbt_save_path]
         return fast_resume_files
 
-    @staticmethod
-    def backup_folder(folder_path, archive_path):
-        logger.info('Creating Archive %s ...' % archive_path)
+    @classmethod
+    def backup_folder(cls, folder_path, archive_path):
+        cls.logger.info('Creating Archive %s ...' % archive_path)
         with zipfile.ZipFile(archive_path, 'w') as archive:
             for file in os.listdir(folder_path):
                 archive.write(os.path.join(folder_path, file))
-        logger.info('Done!')
+        cls.logger.info('Done!')
 
 
 class FastResume(object):
+    logger = logging.getLogger(__name__ + '.FastResume')
     save_path_pattern = br'save_path(\d+)'
     qBt_save_path_pattern = br'qBt-savePath(\d+)'
 
@@ -159,15 +137,15 @@ class FastResume(object):
 
     def set_save_path(self, path: str, target_os: str = None,
                       save_file: bool = True, create_backup: bool = True):
-        path = valid_path(path)
         if target_os is not None:
             path = convert_slashes(path, target_os)
         if create_backup:
             self.save(self.backup_filename)
         path = bytes(path, 'utf-8')
-        logger.debug('Setting save_path... Old: %s, New: %s, Target OS: %s' % (self._qbt_save_path.decode('utf-8'),
-                                                                               path.decode('utf-8'),
-                                                                               target_os))
+        self.logger.debug('Setting save_path... Old: %s, New: %s, Target OS: %s'
+                          % (self._qbt_save_path.decode('utf-8'),
+                             path.decode('utf-8'),
+                             target_os))
         self._data = self._data.replace(
             b'save_path' + bytes(str(len(self._save_path)), 'utf-8') + b':' + self._save_path,
             b'save_path' + bytes(str(len(path)), 'utf-8') + b':' + path
@@ -178,15 +156,15 @@ class FastResume(object):
 
     def set_qbt_save_path(self, path: str, target_os: str = None,
                           save_file: bool = True, create_backup: bool = True):
-        path = valid_path(path)
         if target_os is not None:
             path = convert_slashes(path, target_os)
         if create_backup:
             self.save(self.backup_filename)
         path = bytes(path, 'utf-8')
-        logger.debug('Setting qBt-savePath... Old: %s, New: %s, Target OS: %s' % (self._qbt_save_path.decode('utf-8'),
-                                                                                  path.decode('utf-8'),
-                                                                                  target_os))
+        self.logger.debug('Setting qBt-savePath... Old: %s, New: %s, Target OS: %s'
+                          % (self._qbt_save_path.decode('utf-8'),
+                             path.decode('utf-8'),
+                             target_os))
         self._data = self._data.replace(
             b'qBt-savePath' + bytes(str(len(self._qbt_save_path)), 'utf-8') + b':' + self._qbt_save_path,
             b'qBt-savePath' + bytes(str(len(path)), 'utf-8') + b':' + path
@@ -207,23 +185,6 @@ class FastResume(object):
     def save(self, file_name=None):
         if file_name is None:
             file_name = self.file_path
-        logger.info('Saving File %s...' % file_name)
+        self.logger.info('Saving File %s...' % file_name)
         with open(file_name, 'wb') as f:
             f.write(self._data)
-
-
-if __name__ == '__main__':
-    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
-    logger.setLevel('INFO')
-    qbm = QBTBatchMove()
-    bt_backup = input('BT_Backup Path (%s): ' % qbm.bt_backup_path)
-    if bt_backup.strip() != '':
-        qbm.bt_backup_path = bt_backup
-    ep = input('Existing Path: ')
-    np = input('New Path: ')
-    target = input('Target OS (Windows, Linux, Mac, Blank for same as existing): ')
-    if target.strip() and target.lower() not in ('windows', 'linux', 'mac'):
-        raise ValueError('Target OS is not valid. Must be Windows, Linux, or Mac. Received: %s' % target)
-    elif not target.strip():
-        target = None
-    qbm.run(ep, np, target)
---
+++ qbt_migrate/methods.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def discover_bt_backup_path():
+    logger.debug('Discovering BT_backup path...')
+    if sys.platform.startswith('win32'):
+        logger.debug('Windows System')
+        return os.path.join(os.getenv('localappdata'), 'qBittorrent\\BT_backup')
+
+    logger.debug('Linux/Mac System')
+    return os.path.join(os.getenv('HOME'), '.local/share/data/qBittorrent/BT_backup')
+
+
+def convert_slashes(path: str, target_os: str):
+    if target_os.lower() not in ('windows', 'linux', 'mac'):
+        raise ValueError('Target OS is not valid. Must be Windows, Linux, or Mac. Received: %s' % target_os)
+    if target_os.lower() == 'windows':
+        logger.debug('Convert to Windows Slashes')
+        return path.replace('/', '\\')
+    logger.debug('Convert to Unix Slashes')
+    return path.replace('\\', '/')
